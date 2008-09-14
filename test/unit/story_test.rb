f183afc87a5b81e237bcbbb1c6b0019690d88523
Bob Martens <boblmartens@gmail.com>
2008-09-14 02:00:46 -0500

    Ajax and Web 2.0 (197)

--- test/unit/story_test.rb
+++ test/unit/story_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class StoryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+	def test_should_not_be_valid_without_name
+		s = Story.create(:name => nil, :link => 'http://www.testsubmission/')
+		assert s.errors.on(:name)
+	end
+
+	def test_should_be_valid_without_link
+		s = Story.create(:name => 'My submission test', :link => nil)
+		assert s.errors.on(:link)
+	end
+
+	def test_should_create_story
+		s = Story.create(
+			:name => 'My test submission',
+			:link => 'http://www.testsubmission.com/')
+		assert s.valid?
+	end
 end
