267679876346aa7403035c7eaeb5543606382777
Bob Martens <boblmartens@gmail.com>
2008-09-14 22:13:29 -0500

    Introducing RJS Templates (225)

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,4 +17,8 @@ class StoriesController < ApplicationController
 			render :action => 'new'
 		end
 	end
+
+	def show
+		@story = Story.find(params[:id])
+	end
 end
---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,7 @@
+class VotesController < ApplicationController
+  def create
+		@story = Story.find(params[:story_id])
+		@story.votes.create
+  end
+
+end
