0f429c1db4a7b0ccbfc89faa91ae8ecf2e20f1a3
Bob Martens <boblmartens@gmail.com>
2008-09-26 01:01:33 -0500

    Updating the Style Sheet (370)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
 	map.resources :stories, :has_many => :votes, :collection => { :bin => :get }
 	map.resources :users
+	map.resources :tags
 	map.resource :session
 
   map.root :controller => "stories"
